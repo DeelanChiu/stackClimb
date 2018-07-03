3f590dd54ac742339fc12bd86b4a6d6bc1f5c989
dtc66 <dtc66@cornell.edu>
2018-07-02 23:54:59 -0400

    keys move smiley demo

--- moveRect.py
+++ moveRect.py
@@ -1,5 +1,6 @@
 # import the pygame module, so you can use it
 import pygame
+from pygame.locals import *
 
 # define a main function
 def main():
@@ -15,8 +16,12 @@ def main():
     screen = pygame.display.set_mode((800,520))
     smiley = pygame.image.load('logo32x32.png').convert()
 
+    position = smiley.get_rect()
+
     # define a variable to control the main loop
     running = True
+    move_ticker = 0
+    step = 10
 
     # main loop
     while running:
@@ -27,9 +32,36 @@ def main():
                 # change the value to False, to exit the main loop
                 running = False
 
+        #    if event.type == pygame.KEYDOWN:
+        #        if event.key == pygame.K_LEFT:
+        #            position = position.move(-5, 0)
+        #        if event.key == pygame.K_RIGHT:
+        #            position = position.move(5, 0)
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[K_LEFT] and move_ticker == 0:
+                move_ticker = 1
+                position = position.move(-step, 0)
+            if keys[K_RIGHT] and move_ticker == 0:
+                move_ticker = 1
+                position = position.move(step, 0)
+            if keys[K_UP] and move_ticker == 0:
+                move_ticker = 1
+                position = position.move(0, -step)
+            if keys[K_DOWN] and move_ticker == 0:
+                move_ticker = 1
+                position = position.move(0, step)
+
+            if move_ticker > 0:
+                move_ticker -= 1
+
+
+
         screen.fill((65,105,225))
-        screen.blit(smiley,(0,0))
+        #position = position.move(2, 0)
+        screen.blit(smiley,position)
         pygame.display.update()
+        pygame.time.delay(100)
 
 
 # run the main function only if this module is executed as the main script
